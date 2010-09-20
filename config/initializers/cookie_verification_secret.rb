7dadccc15707addea4d9c47fd118d4d0e1735020
Ivan Rostovsky <ir@anahoret.com>
2010-09-20 17:48:27 +0300

    Firat commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'd2089150ab49af33493be7c678b63172ea6bdfd9beccdfc829c62c5947349bbda92affab45cb7d59ec5fd16bd0ada60c21283c91aae5e2a015307afaec009d45';
